e9ed03bdc263f9df64633cb52b941969a5a911a5
no name no fame <136782390+unknownboiz@users.noreply.github.com>
2025-05-19 19:46:43 +0530

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import time
 from time import mktime
 
 print("Demo for python-discord-rpc Adobe illustrator")
-client_id = '467570845993271307'  # Your application's client ID as a string. (This isn't a real client ID)
+client_id = '1374025974324265032'  # Your application's client ID as a string. (This isn't a real client ID)
 rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)  # Send the client ID to the rpc module
 print("RPC connection successful.")
 
@@ -11,16 +11,16 @@ time.sleep(5)
 start_time = mktime(time.localtime())
 while True:
     activity = {
-            "state": "Adobe",  # anything you like
-            "details": "Editing",  # anything you like
+            "state": "VISUAL STUDIO CODE",  # anything you like
+            "details": "CODING",  # anything you like
             "timestamps": {
                 "start": start_time
             },
             "assets": {
-                "small_text": "Adobe",  # anything you like
-                "small_image": "adobe",  # must match the image key
-                "large_text": "Illustrator",  # anything you like
-                "large_image": "illustrator"  # must match the image key
+                "small_text": "OIAOIA",  # anything you like
+                "small_image": "",  # must match the image key
+                "large_text": "bot.py",  # anything you like
+                "large_image": "geto"  # must match the image key
             }
         }
     rpc_obj.set_activity(activity)
